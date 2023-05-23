9e2cfda2d40d485d3c1073144cdd41ee1ce81398
Fermin <josefermin.hernandez@adevinta.com>
2023-05-23 22:13:26 +0200

    test: setup jest and create dummy tests

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
+        <h1>
           🧗‍♀ Rocking with you
-        </p>
+        </h1>
       </div>
 
       <div className={styles.grid}>
---
+++ web-app/src/tests/index.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../app/page'
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
+
+
+describe('Home', () => {
+    it('renders a heading', () => {
+        render(<Home />)
+
+        const heading = screen.getByRole('heading', {
+            name: /Rocking with you/i,
+        })
+
+        expect(heading).toBeInTheDocument()
+    })
+})
