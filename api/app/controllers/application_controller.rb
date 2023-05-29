b2637fc4387ac02237df0ee1730b709b4ef16cef
Fermin <josefermin.hernandez@adevinta.com>
2023-05-29 21:42:21 +0200

    refactor: extract repository definition to before_action

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -3,29 +3,30 @@
 class ApplicationController < ActionController::API
   OK_MESSAGE = 'ok'
 
+  before_action :initialize_repository
 
   def health_check
     render json: { status: OK_MESSAGE }
   end
 
   def routes
-    # repository = InMemoryRouteRepository.new([]) # to be replaced with a real repository
-    db = SQLite3::Database.new('./db/development.sqlite3')
-    repository = SqliteRouteRepository.new(db)
-    routes = SearchAllRoutesQueryHandler.new(repository).run
-
+    routes = SearchAllRoutesQueryHandler.new(@repository).run
     render json: { data: routes }
   end
 
   def create_route
-    # repository = InMemoryRouteRepository.new([]) # to be replaced with a real repository
-    db = SQLite3::Database.new('./db/development.sqlite3')
-    repository = SqliteRouteRepository.new(db) # to be replaced with a real repository
-    handler = CreateRouteHandler.new(repository)
+    handler = CreateRouteHandler.new(@repository)
     route = handler.run(params[:difficult_level], params[:climbing_time].to_i, params[:comments])
-
     render json: { data: route }
   rescue RouteError => e
     render json: { error: e.message }, status: :bad_request
+  end
+
+  private
+
+  def initialize_repository
+    # repository = InMemoryRouteRepository.new([]) # to be replaced with a real repository
+    db = SQLite3::Database.new('./db/development.sqlite3')
+    @repository = SqliteRouteRepository.new(db)
   end
 end
