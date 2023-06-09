d4999fa54a308ee0f2cf8064d5f74235ee2994b8
Fermin <josefermin.hernandez@adevinta.com>
2023-06-09 18:08:42 +0200

    feature: add openai config

--- api/config/application.rb
+++ api/config/application.rb
@@ -2,7 +2,9 @@
 
 require_relative 'boot'
 
+require 'dotenv/load'
 require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
