f72e1500d327aa17f0e69442be59665295bf4586
Fermin <josefermin.hernandez@adevinta.com>
2023-06-10 18:13:05 +0200

    feature: install mui

--- web-app/pages/_app.tsx
+++ web-app/pages/_app.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import Layout from "@/components/layout/Layout";
 import { Inter } from 'next/font/google';
 import { AppProps } from 'next/app';
 import '@/app/globals.css';
+
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import React from "react";
+
 
 const primaryFont = Inter({
     subsets: ['latin'],
@@ -11,15 +20,21 @@ const primaryFont = Inter({
 export const metadata = {
     title: '🧗‍Rock Climber Login App',
     description: 'Da best! 🤘 Rock Climber Login App 📈',
-}
+};
 
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <main className={`${primaryFont.variable} font-sans`}>
+            <Head>
+                <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+            </Head>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
         </main>
     );
-}
+};
